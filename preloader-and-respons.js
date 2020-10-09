1fa1b8ecb581edfb82a7558c6ef31ab7014e765d
cena1web <60264674+cena1web@users.noreply.github.com>
2020-10-09 19:07:32 +0300

    Create preloader-and-respons.js

---
+++ preloader-and-respons.js
@@ -0,0 +1,17 @@
+<!-- start preloader -->
+<script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'/>
+<script type='text/javascript'>
+$(window).load(function() {
+    $(&quot;.loader&quot;).fadeOut(&quot;slow&quot;);
+})
+</script>
+<!-- end preloader -->
+<!-- start code for make video responsive in all size -->
+<script async='async' src='https://cdnjs.cloudflare.com/ajax/libs/fitvids/1.1.0/jquery.fitvids.min.js' type='text/javascript'/>
+
+<script type='text/javascript'>
+  $(document).ready(function(){
+    $(&quot;.post-body&quot;).fitVids();
+  });
+</script>
+<!-- end code for make video responsive in all size -->
